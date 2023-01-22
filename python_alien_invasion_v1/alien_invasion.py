55e49651d0758eb1317d8dd53875a0247147fcf0
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-21 21:44:22 -0500

    updated the code and removed spaces

--- python_alien_invasion_v1/alien_invasion.py
+++ python_alien_invasion_v1/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 """
     Game class generation and initialization.
     This class is responsible for the game's main loop and
@@ -23,6 +24,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         # set the background color.
+        self.bullets = pygame.sprite.Group()
         self.bg_color = (230, 230, 230)
 
     def run_game(self):
@@ -32,6 +34,12 @@ class AlienInvasion:
             self.check_events()
             self.ship.update()
             self.update_screen()
+            self.bullets.update()
+            # Get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
             # Redraw the screen during each pass through the loop.
             # self.screen.fill(self.bg_color)
             # Make the most recently drawn screen visible.
@@ -59,6 +67,13 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _check_keyup_events(self, event):
         """_summary_
@@ -79,6 +94,8 @@ class AlienInvasion:
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 
