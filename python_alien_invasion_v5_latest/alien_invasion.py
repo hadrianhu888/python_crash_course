96c471348190e033aee27af99069ec8489d307ec
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-24 16:32:17 -0500

    No luck fixing bullets

--- python_alien_invasion_v5_latest/alien_invasion.py
+++ python_alien_invasion_v5_latest/alien_invasion.py
@@ -28,7 +28,7 @@ class AlienInvasion():
             # Watch keyboard and mouse events:
             self.check_events()
             # self.ship.update()
-
+            self.bullets.update()
             # Get rid of bullets that have disappeared:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
--- python_alien_invasion_v5_latest/bullet.py
+++ python_alien_invasion_v5_latest/bullet.py
@@ -1,8 +1,10 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Bullet(Sprite):
     """A class that manages bullets fired from the ship"""
+
     def __init__(self, ai_game):
         """Create a bullet object at the ship's current position"""
         super().__init__()
@@ -11,7 +13,8 @@ class Bullet(Sprite):
         self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0,0) and then set correct position
-        self.rect = pygame.Rect(0,0,self.settings.bullet_width,self.settings.bullet_height)
+        self.rect = pygame.Rect(
+            0, 0, self.settings.bullet_width, self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
         # Store the bullet's position as a decimal value
@@ -26,5 +29,4 @@ class Bullet(Sprite):
 
     def draw_bullet(self):
         """Draw the bullet to the screen"""
-        pygame.draw.rect(self.screen,self.color,self.rect)
-        
+        pygame.draw.rect(self.screen, self.color, self.rect)
