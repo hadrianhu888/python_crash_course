fd7151510f0c1d8e83eda461160396add66ece41
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-31 18:29:16 -0500

    more changes to rw_visual

--- generating_data/rw_visual.py
+++ generating_data/rw_visual.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 # Make a new random walk
-rw = RandomWalk()
-rw.fill_walk()
-# Plot the walk
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-# Emphasize the first and last points
-plt.show()
-plt.savefig('rw_visual.png', bbox_inches='tight')
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    # Plot the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    # Emphasize the first and last points
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1],
+               c='red', edgecolors='none', s=100)
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.show()
+    plt.savefig('rw_visual.png', bbox_inches='tight')
